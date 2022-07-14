b0436a7881488415f943c23e29acd70b9149a19f
Niraj <krajnish114@gmail.com>
2022-07-14 19:08:33 +0530

    message here

---
+++ NodeJSModules/index.js
@@ -0,0 +1,11 @@
+// const average = require("./module");
+// console.log(average([9,4]));
+
+// When you don't provide name you can import module like
+const anyname = require("./module");
+// console.log(anyname.avg([3,4]));
+// console.log(anyname.name);
+// console.log(anyname.city);
+
+// accessing single property exports
+console.log(anyname.movie);
